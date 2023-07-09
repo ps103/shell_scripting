cf05bc2753480f9eb1f61d2cb1e6a8cf8048f770
priyankasharmaa <psharma10394@gmail.com>
2023-07-09 14:53:16 +0530

    welcome message

---
+++ welcome.sh
@@ -0,0 +1,12 @@
+echo "WELCOME MESSAGE APPEARS WHEN CONNECT ec2 ON AWS "
+
+#!/bin/bash
+
+cloud="AWS"
+enviornment="Technical suppport"
+today=$("date")
+echo "I am working on $cloud cloud in era of $enviornment in ZNPL"
+echo "With using $SHELL"
+echo "Last updated on $HOME"
+echo "we are at $HOME and today is "
+echo $today
